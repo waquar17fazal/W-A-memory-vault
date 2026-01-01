9fd8da3ec514f202937d12b025be274d2d046555
waquar17fazal <waquar17fazal@gmail.com>
2026-01-01 19:45:52 +0530

    Add password check function for login

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function checkPassword() {
+  var pass = document.getElementById("password").value;
 
+  if (pass === "1234") {
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  } else {
+    alert("Wrong password");
+  }
+}
